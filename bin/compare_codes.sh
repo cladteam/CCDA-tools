f79d2652989e46a7fac0ef87cdb0149b41571ff7
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-08-12 11:55:42 -0600

    try to get workflows running

--- bin/compare_codes.sh
+++ bin/compare_codes.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for file in output/*; do
+pwd
+for file in snooper_output/*; do
     base_file=$(basename "$file")
     diff "snooper_output/$base_file" "tools_correct_output/$base_file"
 done

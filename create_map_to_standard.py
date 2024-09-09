9e25755c4a89f0b58a75feb2c8befb6bd99d682e
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-09-09 10:53:46 -0600

    pulled vocab dictionaries out into their own file

--- create_map_to_standard.py
+++ create_map_to_standard.py
@@ -22,6 +22,7 @@ import pandas as pd
 import numpy as np
 import argparse
 import os
+import vocab_maps
 
 
 def main():
@@ -45,7 +46,7 @@ def main():
     args = parser.parse_args()
 
     # get vocabularies
-    (oid_map_df, concept_df, concept_relationship_df,source_to_concept_df) = read_vocabulary_tables()
+    (oid_map_df, concept_df, concept_relationship_df,source_to_concept_df) = vocab_maps.read_vocabulary_tables()
 
 
     # Get Data, collect
@@ -157,96 +158,6 @@ def map_code_file(filename, oid_map_df, concept_df, concept_relationship_df,sour
     print('Final')
     print(input_w_standard_df)
     return input_w_standard_df
-
-
-def read_vocabulary_tables():
-    """ returns (oid_map_df, concept_df, concept_relationship_df)
-    """
-    print("READING OID MAP")
-    oid_map_df = pd.read_csv("../CCDA-data/oid.csv", header=0,
-                             on_bad_lines='warn',
-                             dtype={ 'oid': str, 'vocabulary_id': str }
-                             #engine='c', header=0, index_col=0, sep=',',
-                            )
-    print(f"....oid_map {len(oid_map_df)}  {list(oid_map_df)} ")
-
-    print("READING CONCEPT.csv")
-    concept_df = pd.read_csv("../CCDA_OMOP_Private/CONCEPT.csv",
-                             engine='c', header=0, sep='\t',
-                             #index_col=0,
-                             on_bad_lines='warn',
-                             dtype={
-                                    #'concept_id': , 'Int64',
-                                    'concept_id': np.int32 ,
-                                    'concept_name': str,
-                                    'domain_id' : str,
-                                    'vocabulary_id' : str,
-                                    'concept_class_id' : str,
-                                    'standard_concept' : str,
-                                    'concept_code': str,
-                                    'invalid_reason': str,
-                                    'valid_start_date' : str,
-                                    'valid_end_date' : str,
-                                    #'valid_start_date' : some proper date class TBD
-                                    #'valid_end_date' : some proper date class TBD,
-                                },
-                                usecols = [0, 1, 2, 3, 4, 5, 6, 7] # no dates for now
-                             )
-
-    print(f"....have concept_df {len(concept_df)}  {list(concept_df)}")
-
-    print("READING CONCEPT_RELATIONSHIP.csv")
-    concept_relationship_df = pd.read_csv("../CCDA_OMOP_Private/CONCEPT_RELATIONSHIP.csv",
-                             engine='c',
-                             header=0,
-                             # index_col=0,
-                             sep='\t',
-                             on_bad_lines='warn',
-                             dtype={
-                                    #'concept_id': , 'Int64',
-                                    'concept_id_1': np.int32 ,
-                                    'concept_id_2': np.int32 ,
-                                    'relationship_id' : str,
-                                    'valid_start_date' : str,
-                                    'valid_end_date' : str,
-                                    #'valid_start_date' : some proper date class TBD
-                                    #'valid_end_date' : some proper date class TBD,
-                                    'invalid_reason': str
-                                }
-                                #,usecols = [0, 1, 2] # no dates or date types for now
-                             )
-    # ONLY USING "MAPS TO"
-    print(f".....concept_relationship_df {len(concept_relationship_df)} {list(concept_relationship_df)}")
-    print(concept_relationship_df)
-
-    print("===============")
-    #concept_maps_to_df = concept_relationship_df[concept_relationship_df['relationship_id' == 'Maps to']]
-    concept_maps_to_df = concept_relationship_df.query("relationship_id == 'Maps to'")
-
-    print(f".....concept_maps_to_df {len(concept_maps_to_df)} {list(concept_maps_to_df)}")
-    print(concept_maps_to_df)
-
-    print("SOURCE_TO_CONCEPT_MAP.csv")
-    source_to_concept_df = pd.read_csv("../CCDA_OMOP_Private/SOURCE_TO_CONCEPT_MAP.csv",
-                                 engine='c',
-                                 header=0,
-                                 # index_col=0,
-                                 sep='\t',
-                                 on_bad_lines='warn',
-                                 dtype={
-                                        'source_code': str,
-                                        'source_concept_id': np.int32 ,
-                                        'source_vocabulary_id': str ,
-                                        'source_code_description' : str,
-                                        'target_concept_id': np.int32 ,
-                                        'target_vocabulary_id': str ,
-                                        'valid_start_date' : str,
-                                        'valid_end_date' : str,
-                                        'invalid_reason': str
-                                    }
-                                       )
-    print(f"....have source_to_concept_df {len(source_to_concept_df)}  {list(source_to_concept_df)}")
-    return (oid_map_df, concept_df, concept_maps_to_df,source_to_concept_df)
 
 
 

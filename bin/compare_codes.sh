7460beb789bbbc9a0291a3fb9730ef52df7c766d
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-09-12 16:04:11 -0600

    this should do it

--- bin/compare_codes.sh
+++ bin/compare_codes.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-set -euxo pipefail
-pwd
-ls
+set -euo pipefail
 for file in snooper_output/*; do
     base_file=$(basename "$file")
-    echo "snooper_output/$base_file  tools_correct_output/$base_file"
     diff "snooper_output/$base_file" "tools_correct_output/$base_file"
-    if $(( $? gt 0 ))
+    if (( $? > 0 ))
     then
         echo "diff failed for $file with status $?"
     fi

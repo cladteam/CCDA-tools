03d7d2c271aa6699ad8f9e46509e418bed4426e4
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-09-12 15:46:56 -0600

    more detail in compare script

--- bin/compare_codes.sh
+++ bin/compare_codes.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-set -xo pipefail
+###set -xo pipefail
+echo "starting compare"
 pwd
 ls
+echo "comparing files"
 for file in snooper_output/*; do
-    echo "File: \"$file\""
+    echo "File: \"$file\" "
     base_file=$(basename "$file")
     echo "snooper_output/$base_file  tools_correct_output/$base_file"
     diff "snooper_output/$base_file" "tools_correct_output/$base_file"
+    echo "diff status for $base_file $?"
 done

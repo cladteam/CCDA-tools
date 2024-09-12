65f3921e4435a33e11b2cc51a5d911b02b634db2
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-09-12 15:26:02 -0600

    more detail in compare_codes.sh output

--- bin/compare_codes.sh
+++ bin/compare_codes.sh
@@ -3,5 +3,6 @@ set -eou pipefail
 pwd
 for file in snooper_output/*; do
     base_file=$(basename "$file")
+    echo "snooper_output/$base_file  tools_correct_output/$base_file"
     diff "snooper_output/$base_file" "tools_correct_output/$base_file"
 done

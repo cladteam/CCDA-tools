393fd40cbcf3040495f368b928984c586a1c8109
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2024-09-12 15:41:22 -0600

    trying to get this to run or tell me what's wrong

--- bin/compare_codes.sh
+++ bin/compare_codes.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -xo pipefail
 pwd
+ls
 for file in snooper_output/*; do
+    echo "File: \"$file\""
     base_file=$(basename "$file")
     echo "snooper_output/$base_file  tools_correct_output/$base_file"
     diff "snooper_output/$base_file" "tools_correct_output/$base_file"

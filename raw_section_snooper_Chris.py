59a3edd479b86672cc5167d6880096cf7603c2a3
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2024-10-16 19:24:13 +0000

    Autogenerated commit message at Oct 16, 2024 at 17:52

--- raw_section_snooper_Chris.py
+++ raw_section_snooper_Chris.py
@@ -1,87 +1,98 @@
 #!/usr/bin/env python3
 """
-    raw_section_snooper - looks for ANY sections (XML elemnent tagged "section"?),
-        within each type:
-        - entry: ;shows template Id, concepts and names if present,
-        - title
-        - code
-        - value TBD
-
+builds a table of code and value
 """
 
+import os
 import argparse
 import re
 import lxml.etree as ET
 from xml_ns import ns
 from vocab_map_file import oid_map
 import vocab_maps
+import pandas as pd
+from collections import defaultdict
+
+ccda_code_values_columns = [ 'filename', 'section', 'codeSystem', 'code', 
+                             'value-type', 'value-unit',
+                             'value-value', 'value-code', 'value-codeSystem',  'value-text',
+                             'path']
 
 
-def get_code_name(codeSystem, codeSystemName, code, concept_df):
-    if codeSystemName is not None:
-        vocabulary_id  = re.sub(r' CT', '', codeSystemName)
-        concept_query = f"concept_code == '{code}' and vocabulary_id == '{vocabulary_id}'"
-        concept_row = concept_df.query(concept_query)
-        concept_name = 'n/a'
-        if  concept_row.size > 1:
-            concept_name = concept_row['concept_name']
-
-        return concept_name
-    else:
-        return "n/a"
-
-
-def snoop_for_section_tag(tree, expr, filename, concept_df):
-    section_elements = tree.findall(expr, ns)
-
+def snoop_section(tree, filename):
+    trace_df = pd.DataFrame(columns=ccda_code_values_columns)
+    section_elements = tree.findall(".//section", ns)
     for section_element in section_elements:
 
+        section_template_id = "n/a"
+        section_template_ele = section_element.findall("templateId",ns)
+        if len(section_template_ele) > 0:
+            section_template_id = section_template_ele[0].get('root')
+
         entry_elements = section_element.findall("entry", ns)
-
         for entry_ele in entry_elements:
-            code_elements = entry_ele.findall(".//code", ns)
-            for code_ele in code_elements:
-                #concept_name = get_code_name(code_ele.get('code'), code_ele.get('codeSystem'), code_ele.get('codeSystemName'), concept_df)
-                print(( f"        ENTRY-CODE "
-                        f"{re.sub(r'{.*}', '', code_ele.tag)} "
-                        f"{code_ele.get('codeSystem')} "
-                        #f"{code_ele.get('codeSystemName')} "
-                       # f"{code_ele.get('code')} {concept_name}"
-                        ), end="")
-                if code_ele.text is not None:
-                    print(f" {code_ele.text.strip()} ")
-                else:
-                    print("")
-                print(f"          CODE path:\"{re.sub(r'{.*?}', '', tree.getelementpath(code_ele))}\" " )
 
+            value_dict = defaultdict(list)  # keys are paths
             value_elements = entry_ele.findall(".//value", ns)
             for value_ele in value_elements:
-                print( (f"        ENTRY-VALUE {re.sub(r'{.*}', '', value_ele.tag)}"
-                    f" {value_ele.attrib}"), end="")
-                if code_ele.text is not None:
-                    print(f" {value_ele.text}")
-                else:
-                    print("")
-                print(f"          VALUE path:\"{re.sub(r'{.*?}', '', tree.getelementpath(code_ele))}\" " )
+                value_path = re.sub(r'{.*?}', '', tree.getelementpath(value_ele))
+                value_path = "/".join(value_path.split("/")[:-1])
+                value_attribs_dict = { re.sub(r'{.*}', '', a):
+                                       re.sub(r'{.*}', '', value_ele.attrib[a])
+                                       for a in value_ele.attrib  }
+                value_dict[value_path].append( (value_attribs_dict, value_ele.text) )
+
+            code_elements = entry_ele.findall(".//code", ns)
+            for code_ele in code_elements:
+                code_path = re.sub(r'{.*?}', '', tree.getelementpath(code_ele))
+                code_path = "/".join(code_path.split("/")[:-1])
+                value_tuple_list = value_dict[code_path] # tuple is (dict, text)
+                for value_tuple in value_tuple_list:
+                    new_row = pd.DataFrame([{
+                        'filename': filename,
+                        'section': section_template_id, 
+                        'path': code_path,
+                        'code': code_ele.get('code'),
+                        'codeSystem': code_ele.get('codeSystem'),
+                        'value-type': value_tuple[0].get('type', ''),
+                        'value-unit': value_tuple[0].get('unit', ''),
+                        'value-value': value_tuple[0].get('value', ''),
+                        'value-code': value_tuple[0].get('code', ''),
+                        'value-codeSystem': value_tuple[0].get('codeSystem', ''),
+                        'value-text': value_tuple[1].strip() if value_tuple[1] else ''
+                    }])
+                    trace_df = pd.concat([trace_df, new_row], ignore_index=True)
+    return(trace_df)
+
 
 def main():
+    all_files_df = pd.DataFrame(columns=ccda_code_values_columns)
     parser = argparse.ArgumentParser(
         prog='CCDA - OMOP Code Snooper',
         description="finds all code elements and shows what concepts the represent",
         epilog='epilog?')
-    parser.add_argument('-f', '--filename', required=True, help="filename to parse")
+    parser.add_argument('-f', '--filename', help="filename to parse")
+    parser.add_argument('-d', '--directory', help="directory of files to parse")
     args = parser.parse_args()
 
-    #print("Reading Vocab, takes a minute")
-    #concept_df = vocab_maps.read_concept()
-    concept_df = None
-    tree = ET.parse(args.filename)
-
-    # regular section
-    #snoop_for_section_tag(tree, "code", args.filename)
-    snoop_for_section_tag(tree, ".//section", args.filename, concept_df)
-
-    # section with organizer
+    if args.filename is not None:
+        tree = ET.parse(args.filename)
+        file_df = snoop_section(tree, args.filename)
+        #pd.set_option('display.max_rows', len(file_df))
+        print(file_df)
+    elif args.directory is not None: 
+        only_files = [f for f in os.listdir(args.directory) if os.path.isfile(os.path.join(args.directory, f))]
+        for filename in (only_files):
+            if filename.endswith(".xml"):
+                tree = ET.parse(os.path.join(args.directory, filename))
+                file_df = snoop_section(tree, filename)
+                all_files_df = pd.concat([all_files_df, file_df], ignore_index=True)
+        #pd.set_option('display.max_rows', len(all_files_df))
+        print(all_files_df) 
+        all_files_df.to_csv(f"raw_section_snooper_Chris.csv",
+                                sep=",", header=True, index=False)
+    else:
+        logger.error("Did args parse let us  down? Have neither a file, nor a directory.")
 
 
 
